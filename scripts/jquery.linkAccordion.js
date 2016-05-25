45659651be1d386151cb056d2452f2e9af01cb34
Christopher Torgalson <manager@bedlamhotel.com>
2016-05-25 13:03:31 +0200

    Brings comments into line with modern coding style

--- scripts/jquery.linkAccordion.js
+++ scripts/jquery.linkAccordion.js
@@ -10,24 +10,32 @@
  *
  * @version 2.2
  * @author Christopher Torgalson <manager@bedlamhotel.com>
- * @param object overrides Configuration options:
  *
- * -- closedClass: (string) Class for closed toggle link
- * -- closeText: (string) Text for open state of toggle link
- * -- defaultContent: (int) [zero-based] index of accordion item to open
- *    by default. Uses jQuery's .eq()/:eq() so that positive integers count from
- *    the beginning of the collection and negative integers count from the end
- *    of the collection. To open NO items by default use an integer larger than
- *    the size of the jQuery collection being operated on.
- * -- headingElement: (string [css selector]) The heading element to append
- *    toggle links to (must immediately precede the element to be toggled
- * -- linkHeadings: (bool) Whether to link the headings directly or to insert a
- *    toggle link
- * -- openedClass: (string) Class for open toggle link/heading
- * -- openText: (string) Text for closed state of toggle link/heading
- * -- slideToggle: (object) configuration options for jQuery .slideToggle()
- *    function
- * -- toggleLinkClass: (string) Class for toggle link
+ * @param {object} overrides
+ *   Configuration options.
+ * @param {string} overrides.closedClass
+ *   Class for closed toggle link.
+ * @param {string} overrides.closeText
+ *   Text for open state of toggle link.
+ * @param {int} overrides.defaultContent
+ *   Zero-based index of accordion item to open by default. Uses jQuery's
+ *   .eq()/:eq() so that positive integers count from the beginning of the
+ *   collection and negative integers count from the end of the collection.
+ *   To open NO items by default use an integer larger than the size of the
+ *   jQuery collection being operated on.
+ * @param {string} overrides.headingElement
+ *   The heading element to append toggle links to (should be acss selector;
+ *   must immediately precede the element to be toggled).
+ * @param {bool} overrides.linkHeadings
+ *   Whether to link the headings directly or to insert a toggle link.
+ * @param {string} overrides.openedClass
+ *   Class for open toggle link/heading.
+ * @param {string} overrides.openText
+ *   Text for closed state of toggle link/heading.
+ * @param {object} overrides.slideToggle
+ *   Configuration options for jQuery .slideToggle() function.
+ * @param {string} overrides.toggleLinkClass
+ *   Class for toggle link.
  *
  * @see http://api.jquery.com/slideToggle/
  * @see http://api.jquery.com/eq/
@@ -50,14 +58,20 @@
       var $current = $(e),
           // Get the headings...
           $headings = $current
-            .find(settings.headingElement) // Get the headings...
-            .addClass(settings.closedClass) // Give them all the closed class...
-            .eq(settings.defaultContent) // Reduce collection to default...
-            .addClass(settings.openedClass) // Add opened class to this item...
-            .end(), // Return to full collection...
+            // Get the headings.
+            .find(settings.headingElement)
+            // Give them all the closed class.
+            .addClass(settings.closedClass)
+            // Reduce collection to default.
+            .eq(settings.defaultContent)
+            // Add opened class to this item.
+            .addClass(settings.openedClass)
+            // Return to full collection.
+            .end(),
           // Get the corresponding contents:
           $contents = $headings.next().not(':eq(' + settings.defaultContent + ')').hide().end(),
-          clickHandler = function(e) { // Add click handler...
+          // Add click handler.
+          clickHandler = function(e) {
             // We don't know, in this context, what element the click handler
             // will be attached to. This means to that to determin the heading,
             // we need to test the kind of element that's just been clicked each
@@ -88,7 +102,6 @@
             }
             e.preventDefault();
           };
-          //
           if (settings.linkHeadings) {
             $headings.click(clickHandler);
           }
